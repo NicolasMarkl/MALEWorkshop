d4ef94e1804a59ce288b64c566d41cd6a75d7c29
NicolasMarkl <55098610+NicolasMarkl@users.noreply.github.com>
2022-12-19 20:20:19 +0100

    update one error otherwise it should be good

--- app.R
+++ app.R
@@ -7,7 +7,17 @@
 #    http://shiny.rstudio.com/
 #
 
+
+
 library(shiny)
+library(ggplot2)
+library(tidyverse)
+#library(rsconnect)
+#rsconnect::setAccountInfo(name='wi20b037', token='4DFB03C6A510B0BDF59222FC835B6D87', secret='mJmUptE8nCI3HjU9Un/2hiiKNwkg4CDeUXeMisGm')
+
+
+# Load the iris dataset as an example
+data(iris)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -16,34 +26,64 @@ ui <- fluidPage(
     titlePanel("Old Faithful Geyser Data"),
 
     # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          numericInput("mpg", "MPG", 20),
+          numericInput("cyl", "Cylinders", 4),
+          numericInput("disp", "Displacement", 100),
+          numericInput("hp", "Horsepower", 80),
+          numericInput("drat", "Rear axle ratio", 3.5),
+          numericInput("wt", "Weight", 2.0),
+          numericInput("qsec", "1/4 mile time", 16.5),
+          
+          # Button to submit the input values
+          actionButton("predict", "Predict"),
+          
+          # Output displaying the cluster prediction
+          textOutput("prediction")
         )
     )
-)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  
+  # Load the mtcars data and normalize it
+  data <- mtcars %>% 
+    select(mpg, cyl, disp, hp, drat, wt, qsec) %>%
+    scale()
+  
+  # Run the k-Means model
+  kmeans_model <- reactive({
+    kmeans(data, centers = 3)
+  })
+  
+  # Define a reactive value for the cluster prediction
+  prediction <- reactiveValues(pred = NULL)
+  
+  # Make the cluster prediction when the button is clicked
+  observeEvent(input$predict, {
+    # Define the new data
+    new_data <- data.frame(mpg = input$mpg, cyl = input$cyl, disp = input$disp, hp = input$hp, drat = input$drat, wt = input$wt, qsec = input$qsec)
+    
+    # Normalize the new data
+    new_data_scaled <- scale(new_data)
+    
+    # Make the cluster prediction
+    prediction$pred <- predict(kmeans_model(), new_data_scaled)
+  })
+  
+  # Output the cluster prediction
+  output$prediction <- renderText({
+    if (!is.null(prediction$pred)) {
+      paste("Prediction: Cluster", prediction$pred)
+    } else {
+      "No prediction yet."
+    }
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+# Deploy the application
+#rsconnect::deployApp()
